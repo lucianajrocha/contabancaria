9efcf23a04f4c861016134ac2a6bf46b4a3802d8
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-14 16:08:06 -0300

    implementando o projeto

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -2,11 +2,24 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.model.Conta;
+
 import conta.util.Cores;
 
 public class Menu {
 
     public static void main(String[] args) {
+
+        // Teste da Classe Conta;
+        Conta c1 = new Conta(1, 123, 1, "Adriana", 1000.0f);
+
+
+        c1.visualizar();
+        c1.sacar(12000.0f);
+        c1.visualizar();
+        c1.depositar(5000.0f);
+        c1.visualizar();
+
 
         Scanner leia = new Scanner(System.in);
         int opcao;
