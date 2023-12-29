b491b793d2bfd8f547541e6a722dd6905294ab65
Luciana Rocha <lucianajrocha97@gmail.com>
2023-12-29 08:16:46 -0300

    implementando o codigo

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -240,9 +240,9 @@ public class Menu {
 
     public static void sobre() {
         System.out.println("\n*********************************************************");
-        System.out.println("Projeto Desenvolvido por: ");
-        System.out.println("Generation Brasil - generation@generation.org");
-        System.out.println("github.com/conteudoGeneration");
+        System.out.println("Projeto Desenvolvido por: Luciana Jovani Rocha");
+        System.out.println("Generation Brasil - lucianajrocha@hotmail.com");
+        System.out.println("github.com/lucianajrocha");
         System.out.println("*********************************************************");
     }
 
